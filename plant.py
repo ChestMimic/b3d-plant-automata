feab8c3547130c8cbae38548afbcaceef7816d41
ibbolia <ibbolia@gmail.com>
2017-02-20 11:20:38 -0500

    First Commit

---
+++ plant.py
@@ -0,0 +1,9 @@
+bl_info = {
+	"name":"Plant Automata",
+	"description":"Generate plants",
+	"version":(0,1,0),
+	"blender":(2,78,0),
+	"support":"TESTING",
+	"category":"Object",
+	"author":"Mark Fitzgibbon"
+}
